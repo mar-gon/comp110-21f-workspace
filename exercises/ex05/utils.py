0f2a90d96843a35c6fd8ad115e00a6e2e13daa27
Mariana Gonzalez <margon@ad.unc.edu>
2021-10-06 00:27:58 -0400

    EX05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,16 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730394836"
 
-# Define your functions below
+
+def only_evens(nums: list[int]) -> list[int]:
+    """Returns only the even numbers of a list."""
+    i: int = 0
+    evens: list[int] = []
+    while i < len(nums):
+        if (nums[i] % 2) == 0:
+            evens.append(nums[i])
+        else: 
+            return evens
+        i += 1
+    return evens
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,29 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
+# in terminal: python -m pytest exercises/ex05
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730394836"
+
+from exercises.ex05.utils import only_evens
+
+
+def test_only_evens1() -> None:
+    """Tests if it returns evens."""
+    lis: list[int] = [1, 2, 3, 4]
+    i: int = 0
+    while i < len(only_evens(lis)):
+        assert (only_evens(lis)[i] % 2) == 0
+        i += 1
+
+
+def test_only_evens2() -> None:
+    """Tests if it returns blank if all odds."""
+    lis: list[int] = [1, 3, 5]
+    assert only_evens(lis) == []
+
+
+def test_only_evens3() -> None:
+    """Tests an empty list."""
+    lis: list[int] = []
+    assert only_evens(lis) == []
