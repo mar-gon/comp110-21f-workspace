ac9f7376cd57facb9b0254d0fb527f521cacca2c
Mariana Gonzalez <margon@ad.unc.edu>
2021-10-06 20:55:11 -0400

    EX05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -14,3 +14,42 @@ def only_evens(nums: list[int]) -> list[int]:
             return evens
         i += 1
     return evens
+
+
+def sub(original: list[int], start_index: int, end_index: int) -> list[int]:
+    """Returns list with items between start index and end index - 1."""
+    new: list[int] = []
+    i: int = 0
+    if len(original) == 0:
+        return new
+    if start_index > len(original):
+        return new
+    if end_index <= 0:
+        return new
+    if start_index < 0:
+        while i < end_index:
+            new.append(original[i])
+            i += 1
+    if end_index < len(original):
+        while start_index < end_index:
+            new.append(original[start_index])
+            start_index += 1 
+    if end_index > len(original):
+        while start_index < len(original):
+            new.append(original[start_index])
+            start_index += 1 
+    return new
+
+
+def concat(first: list[int], second: list[int]) -> list[int]:
+    """Returns a list with the items of the first parameter list followed by the second's items."""
+    both: list[int] = []
+    i: int = 0 
+    while i < len(first):
+        both.append(first[i])
+        i += 1
+    i = 0
+    while i < len(second):
+        both.append(second[i])
+        i += 1
+    return both
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,11 +1,12 @@
 """Unit tests for list utility functions."""
 
 # in terminal: python -m pytest exercises/ex05
-# from exercises.ex05.utils import only_evens, sub, concat
 
 __author__ = "730394836"
 
 from exercises.ex05.utils import only_evens
+from exercises.ex05.utils import sub
+from exercises.ex05.utils import concat
 
 
 def test_only_evens1() -> None:
@@ -27,3 +28,42 @@ def test_only_evens3() -> None:
     """Tests an empty list."""
     lis: list[int] = []
     assert only_evens(lis) == []
+
+
+def test_sub1() -> None:
+    """Tests if it returns subset list of items between start index & end index - 1."""
+    a_list: list[int] = [10, 20, 30, 40]
+    assert sub(a_list, 1, 3) == [20, 30]
+
+
+def test_sub2() -> None:
+    """Tests if it includes num @ end index if its greater than the len of original list."""
+    a_list: list[int] = [10, 20, 30, 40]
+    assert sub(a_list, 1, 5) == [20, 30, 40]
+
+
+def test_sub3() -> None:
+    """Tests if when lenth of original list is zero, sub returns an empty list."""
+    a_list: list[int] = []
+    assert sub(a_list, 1, 3) == []
+
+
+def test_concat1() -> None:
+    """Tests if it returns list including first list items followed by second list items."""
+    first: list[int] = [1, 2, 3]
+    second: list[int] = [4, 5, 6]
+    assert concat(first, second) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat2() -> None:
+    """Tests if it returns empty list when both argument lists are empty."""
+    first: list[int] = []
+    second: list[int] = []
+    assert concat(first, second) == []
+
+
+def test_concat3() -> None:
+    """Tests if it returns only second argument if first argument is empty."""
+    first: list[int] = []
+    second: list[int] = [1, 2, 3]
+    assert concat(first, second) == [1, 2, 3]
