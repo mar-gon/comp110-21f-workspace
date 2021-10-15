bd449f091e157ac0c947dd377964fa5162913fee
Mariana Gonzalez <margon@ad.unc.edu>
2021-10-15 19:35:07 -0400

    LS19

---
+++ lessons/helpers.py
@@ -0,0 +1,6 @@
+"""Example of fuctions imported elsewhere."""
+
+def powerful(x: float, n: float) -> float:
+    """Raise x to the power of n."""
+    return x ** n
+
---
+++ lessons/imports.py
@@ -0,0 +1,5 @@
+"""Examples of imports."""
+
+from lessons import helpers
+
+print(helpers.powerful(2, 4))
