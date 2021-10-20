15d74a71e06d447295c0b4f9a836939ec2da61fc
Mariana Gonzalez <margon@ad.unc.edu>
2021-10-19 22:53:40 -0400

    EX06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,72 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730394836"
 
-# Define your functions below
+
+def invert(old: dict[str, str]) -> dict[str, str]:
+    """Inverts keys and values."""
+    new: dict[str, str] = dict()
+    for key in old:
+        new[old[key]] = key
+        if new == KeyError:
+            raise KeyError("You cant have two keys or values twice.")
+    return new
+
+
+def favorite_colors(ppl: dict[str, str]) -> str:
+    """Returns most frequent color."""
+    red: int = 0
+    orange: int = 0
+    yellow: int = 0
+    green: int = 0
+    blue: int = 0
+    purple: int = 0
+    pink: int = 0
+    if ppl == {}:
+        return "No favorite colors."
+    for person in ppl:
+        if ppl[person] == "red":
+            red += 1
+        if ppl[person] == "orange":
+            orange += 1
+        if ppl[person] == "yellow":
+            yellow += 1
+        if ppl[person] == "green":
+            green += 1
+        if ppl[person] == "blue":
+            blue += 1
+        if ppl[person] == "purple":
+            purple += 1
+        if ppl[person] == "pink":
+            pink += 1
+    if red == 1 and orange == 1 and yellow == 1 and green == 1 and blue == 1 and purple == 1 and pink == 1:
+        return "No favorite colors."
+    if red > orange and red > yellow and red > green and red > blue and red > purple and red > pink:
+        return "red"
+    if orange > red and orange > yellow and orange > green and orange > blue and orange > purple and orange > pink:
+        return "orange"
+    if yellow > orange and yellow > red and yellow > green and yellow > blue and yellow > purple and yellow > pink:
+        return "yellow"
+    if green > orange and green > yellow and green > red and green > blue and green > purple and green > pink:
+        return "green"
+    if blue > orange and blue > yellow and blue > green and blue > red and blue > purple and blue > pink:
+        return "blue"
+    if purple > purple > orange and purple > yellow and purple > green and purple > blue and purple > red and purple > pink:
+        return "purple"
+    return "pink"
+
+
+def count(lis: list[str]) -> dict[str, int]:
+    """Counts the number of times an item appears in the list."""
+    dic: dict[str, int] = {}
+    i: int = 0
+    if lis == []:
+        return dic
+    while i < len(lis):
+        for item in dic:
+            if lis[i] in dic:
+                dic[lis[i]] += 1
+            else:
+                dic[lis[i]] = 1
+        i += 1
+    return dic
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,63 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+# python -m pytest exercises/ex06
 
 __author__ = "123456789"
+
+from exercises.ex06.dictionaries import invert
+from exercises.ex06.dictionaries import favorite_colors
+from exercises.ex06.dictionaries import count
+
+
+def test_invert1() -> None:
+    """Tests if it works with an empty value."""
+    dic: dict[str, str] = {"a": ""}
+    assert invert(dic) == {"": "a"}
+
+
+def test_invert2() -> None:
+    """Tests dictionary with one key."""
+    dic: dict[str, str] = {"a": "x"}
+    assert invert(dic) == {"x": "a"}
+
+
+def test_invert3() -> None:
+    """Tests dictionary with multiple keys."""
+    dic: dict[str, str] = {"a": "x", "b": "y", "c": "z"}
+    assert invert(dic) == {"x": "a", "y": "b", "z": "c"}
+
+
+def test_favorite_colors1() -> None:
+    """Tests if function works normally."""
+    dic: dict[str, str] = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue", "Mariana": "pink"}
+    assert favorite_colors(dic) == "blue"
+
+
+def test_favorite_colors2() -> None:
+    """Tests if it returns 'no favorite colors' if no colors are repeated."""
+    dic: dict[str, str] = {"Marc": "yellow", "Ezri": "green", "Kris": "blue", "Mariana": "pink", "Joe": "purple", "Lizzie": "red", "Pou": "orange"}
+    assert favorite_colors(dic) == "No favorite colors."
+
+
+def test_favorite_colors3() -> None:
+    """Tests if it returns 'no favorite colors' if empty dict in argument."""
+    dic: dict[str, str] = {}
+    assert favorite_colors(dic) == "No favorite colors."
+
+
+def test_count1() -> None:
+    """Tests if function works normal."""
+    items: list[str] = ["apple", "carrot", "tomato", "carrot", "apple"]
+    assert count(items) == {"apple": 2, "carrot": 2, "tomato": 1}
+
+
+def test_count2() -> None:
+    """Tests when list only has one item."""
+    items: list[str] = ["apple"]
+    assert count(items) == {"apple": 1}
+
+
+def test_count3() -> None:
+    """Tests when given an empty list returns empty."""
+    items: list[str] = []
+    assert count(items) == {}
